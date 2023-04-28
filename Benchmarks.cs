54d6532dbad52216e64d9a1e45df0c07d4ab9924
Vitor Santos <vsantos@alter-solutions.com>
2023-04-28 12:18:33 +0100

    feat: benchmark of mapping methods

---
+++ Benchmarks.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using BenchmarkDotNet.Attributes;
+
+namespace MapperBenchmarks
+{
+    public class Benchmarks
+    {
+
+        private Product[] _products;
+        private IMapper _mapper; 
+
+        //[Params(10, 100, 1000)]
+        [Params(10, 100)]
+        public int NumberOfElements { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Product, ProductDto>()
+                    .ForMember(dest => dest.ProductName, s => s.MapFrom(src => src.ProductName))
+                    .ForMember(dest => dest.ProductDescription, s => s.MapFrom(src => src.ProductDescription))
+                    .ForMember(dest => dest.Price, s => s.MapFrom(src => src.Price + src.Price * 100 / src.VatPercentage));
+            });
+            
+            _mapper = config.CreateMapper();
+
+            _products = Enumerable.Range(1, NumberOfElements)
+                                    .Select(x => new Product 
+                                    {
+                                        Id = x,
+                                        ProductCategoryName = $"Product  Number {x}",
+                                        ProductDescription = $"Product  Description {x}",
+                                        Price = 45.5m,
+                                        VatPercentage = 19
+                                    }).ToArray();
+        }
+
+        [Benchmark]
+        public void WithAutoMapper() 
+        {
+            foreach (var product in _products)
+            {
+                var productDto = _mapper.Map<ProductDto>(product);
+            }
+        }
+
+        [Benchmark]
+        public void WithDirectAssignment()
+        {
+            foreach (var product in _products)
+            {
+                var productDto = ProductDto.ConvertFromProduct(product);
+            }
+        }
+
+        [Benchmark]
+        public void WithExplicitCast()
+        {
+            foreach (var product in _products)
+            {
+                var productDto = (ProductDto)product;
+            }
+        }
+    }
+}
---
+++ ProductDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapperBenchmarks
+{
+    public class ProductDto
+    {
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+
+        public static implicit operator Product(ProductDto productDto) => new Product
+        {
+            ProductName = productDto.ProductName,
+            ProductDescription = productDto.ProductDescription,
+            Price = productDto.Price
+        };
+
+        public static explicit operator ProductDto(Product product) => new ProductDto
+        {
+            ProductName = product.ProductName,
+            ProductDescription = product.ProductDescription,
+            Price = product.Price + product.Price * 100 / product.VatPercentage,
+        };
+
+        public static ProductDto ConvertFromProduct(Product product)
+        {
+            return new ProductDto
+            {
+                ProductName = product.ProductName,
+                ProductDescription = product.ProductDescription,
+                Price = product.Price + product.Price * 100 / product.VatPercentage,
+            };
+        }
+    }
+}
